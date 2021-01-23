155685f809475e8dd1f4efc483d819d23a5d40ee
TemaSaur <TemaSaur@gmail.com>
2021-01-23 21:49:31 +0500

    add sorting

--- js/term.js
+++ js/term.js
@@ -1,11 +1,36 @@
 const $_GET = location.search.substr(1).split("&").reduce((o,i)=>(u=decodeURIComponent,[k,v]=i.split("="),o[u(k)]=v&&u(v),o),{});
 const term = $_GET.term
 console.log(term)
+const data = []
 
 const termName = document.querySelector(".term")
 const categories = document.querySelector(".categories")
 const definition = document.querySelector(".definition")
 const instance = document.querySelector(".instance")
+
+
+async function getJSON(path) {
+	const res = await fetch(path)
+	const jason = await res.json()
+	for (let i = 0; i < jason.length; ++i) {
+		data.push(jason[i])
+	}
+}
+
+function bisect(data, x) {
+	let lo = 0, 
+			hi = data.length,
+			mid;
+	console.log([lo, hi])
+	while (lo < hi) {
+		mid = Math.floor((lo + hi) / 2)
+		if (data[mid].name < x)
+			lo = mid + 1
+		else
+			hi = mid
+	}
+	return data[lo];
+}
 
 function addItem(obj) {
 	termName.innerHTML = obj.name;
@@ -15,20 +40,15 @@ function addItem(obj) {
 	}
 	const string = obj.definition.split("\n")
 	console.log(string)
-	// definition.innerHTML = obj.definition
-	// definition.innerHTML = ""
-	// definition.innerHTML += string + "<br>"
 	definition.innerHTML = string.join("</p><p class=\"definition\">")
 	instance.innerHTML = obj.instance
 }
 
-async function getJSON(path) {
-	const res = await fetch(path)
-	const jason = await res.json()
-	for (let i = 0; i < jason.length; ++i) {
-		if (jason[i].name == term)
-			addItem(jason[i])
-	}
-}
 
-getJSON("/js/terms.json")
+getJSON("/js/terms.json").then(() => {
+	termObject = bisect(data, term)
+	addItem(termObject)
+})
+// console.log(data)
+// const termObject = bisect(data, term)
+// addItem(termObject)
